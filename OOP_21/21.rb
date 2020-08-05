dbca05ff16f9100a1e12ecaafe551b3ea2555957
Chris Salazar <chrissalazarem@icloud.com>
2020-08-05 12:35:56 -0700

    21 GAME

--- OOP_21/21.rb
+++ OOP_21/21.rb
@@ -1,24 +1,27 @@
+# CLASS FOR DECK OF CARDS
 class Deck
   def initialize
     @cards = new_deck
   end
 
   def new_deck
-    Card::CARD_SUITS.product(Card::CARD_RANKS).map {|card_info| Card.new(card_info)}
+    new_set = Card::CARD_SUITS.product(Card::CARD_RANKS)
+    new_set.map { |card_info| Card.new(card_info) }
   end
-  
-  def pull_card(num=1)
+
+  def pull_card(num = 1)
     pulled = @cards.sample(num)
-    pulled.each {|card| @cards.delete(card)}
+    pulled.each { |card| @cards.delete(card) }
     pulled
   end
 end
 
+# CLASS FOR EACH INDIVIDUAL CARD OBJECT IN DECK
 class Card
-attr_reader :suit, :rank
+  attr_reader :suit, :rank
 
-CARD_RANKS = (2..10).to_a + ['J', 'Q', 'K', 'A']
-CARD_SUITS = ['S', 'D', 'C', 'H']
+  CARD_RANKS = ((2..10).to_a + %w(J Q K A)).freeze
+  CARD_SUITS = %w(S D C H).freeze
 
   def initialize(card_info)
     @suit = card_info[0]
@@ -29,7 +32,7 @@ CARD_SUITS = ['S', 'D', 'C', 'H']
     rank == 'Q'
   end
 
-  def king? 
+  def king?
     rank == 'K'
   end
 
@@ -37,7 +40,7 @@ CARD_SUITS = ['S', 'D', 'C', 'H']
     rank == 'J'
   end
 
-  def ace? 
+  def ace?
     rank == 'A'
   end
 
@@ -50,21 +53,21 @@ CARD_SUITS = ['S', 'D', 'C', 'H']
   end
 
   def convert_suit
-    ['Spades', 'Diamonds', 'Clubs', 'Hearts'].select {|str| str[0] == @suit[0]}[0]
+    %w(Spades Diamonds Clubs Hearts).select { |str| str[0] == @suit[0] }[0]
   end
 
-  def convert_rank  
+  def convert_rank
     if honor_card?
-      ['Jack', 'Queen', 'King', 'Ace'].select {|str| str[0] == @rank[0]}[0]
+      %w(Jack Queen King Ace).select { |str| str[0] == @rank[0] }[0]
     else
       @rank
     end
   end
-
 end
 
+# GENERAL CLASS FOR COMPUTER / PLAYER
 class Participant
-  attr_accessor :hand
+  attr_accessor :hand, :name
 
   def initialize
     @hand = []
@@ -80,17 +83,17 @@ class Participant
   end
 
   def hand_value
-    aces = hand.select {|card| card.ace?}
-    aces.map!.with_index {|obj, idx| idx == 0 ? 11 : 1 }
+    aces = hand.select { |card| card.ace? }
+    aces.map!.with_index { |_, idx| idx == 0 ? 11 : 1 }
 
-    others = hand.select {|card| !card.ace?}
-    others.map! {|card| card.honor_card? ? 10 : card.rank }
+    others = hand.select { |card| !card.ace? }
+    others.map! { |card| card.honor_card? ? 10 : card.rank }
 
     aces.sum + others.sum
   end
 
   def display_cards
-    @hand.map {|card| card.display}
+    @hand.map { |card| card.display }
   end
 
   def bust?
@@ -101,30 +104,31 @@ class Participant
     @hand = []
   end
 end
-
+# Humans Class
 class Player < Participant
   def set_name
     name = ''
-    puts "Please enter your name:"
+    puts 'Please enter your name:'
     loop do
       name = gets.chomp.capitalize
       break if name != ''
-      puts "Please enter a name."
+      puts 'Please enter a name.'
     end
     @name = name
   end
 end
-
+# Computer Class for Dealer
 class Dealer < Participant
   def set_name
-    @name = ['R2D2', 'ROBOT', '21BOT', 'LSBOT']
+    @name = %w(R2D2 ROBOT BOT LSBOT).sample
   end
 
   def have_17?
     hand_value >= 17
-  end 
+  end
 end
 
+# MAIN GAME CLASS
 class Game
   attr_reader :deck, :human, :dealer
 
@@ -144,7 +148,7 @@ class Game
   def clear_terminal
     system('clear') || system('cls')
   end
-  
+
   def clear_hands
     human.clear_hand
     dealer.clear_hand
@@ -179,30 +183,30 @@ class Game
   end
 
   def line
-    '-'*50
+    '-' * 50
   end
 
   def joinor(cards)
     case cards.size
     when 1 then cards.join(', ')
-    when 2 then cards.join (' and ')
+    when 2 then cards.join(' and ')
     else cards[0..-2].join(', ') + ' and ' + cards[-1]
     end
   end
 
   def display_welcome_message
-    prompt("Welcome to the game of TWENTY-ONE!")
+    prompt('Welcome to the game of TWENTY-ONE!')
     prompt(line)
   end
 
   def start_game?
-    prompt("Press ENTER to start the game!")
+    prompt('Press ENTER to start the game!')
     gets.chomp
   end
 
   def next_card
-    prompt("Dealer is going to HIT!")
-    prompt("Press ENTER to see next card dealt!")
+    prompt("#{dealer.name} is going to HIT!")
+    prompt('Press ENTER to see next card dealt!')
     gets.chomp
   end
 
@@ -217,14 +221,14 @@ class Game
     unknown_sentence = dealer.display_cards[0] + ' and unknown card.'
     visible = joinor(dealer.display_cards)
    
-    prompt("DEALER's hand is #{unknown ? unknown_sentence : visible}")
-    prompt("DEALER's hand value is #{dealer.hand_value}") if value
+    prompt("#{dealer.name}'s hand is #{unknown ? unknown_sentence : visible}")
+    prompt("#{dealer.name}'s hand value is #{dealer.hand_value}") if value
     prompt(line)
   end
 
   def hit_or_stay?
     message = "Would you like to hit or stay?"
-    two_option_answer(message, ['h', 's'])
+    two_option_answer(message, %w(h s))
   end
 
   def nobody_bust
@@ -233,29 +237,29 @@ class Game
 
   def display_winner_or_tie
     case human.hand_value <=> dealer.hand_value
-    when 1 then prompt("YOU WON! #{human.hand_value} - #{dealer.hand_value}")
-    when -1 then prompt("DEALER WON! #{dealer.hand_value} - #{human.hand_value}")
+    when 1 then prompt("#{human.name} WON! #{human.hand_value} - #{dealer.hand_value}")
+    when -1 then prompt("#{dealer.name} WON! #{dealer.hand_value} - #{human.hand_value}")
     else prompt("IT WAS A TIE! #{human.hand_value} - #{dealer.hand_value}")
     end
   end
 
   def display_results
     prompt("YOU BUSTED! YOU LOSE!") if human.bust?
-    prompt("DEALER BUSTED! DEALER LOSES!") if dealer.bust?
+    prompt("#{dealer.name} BUSTED! DEALER LOSES!") if dealer.bust?
     display_winner_or_tie if nobody_bust
   end
 
   def play_again?
-    mssg = "Would you like to play again? (Y or N):"
-    two_option_answer(mssg, ['y', 'n']) == 'n'
+    mssg = 'Would you like to play again? (Y or N):'
+    two_option_answer(mssg, %w(y n)) == 'n'
   end
 
   def display_goodbye_message
-    prompt("GOODBYE! THANKS FOR PLAYING!")
+    prompt('GOODBYE! THANKS FOR PLAYING!')
   end
 
   def human_loop
-    loop do #HUMAN LOOP START
+    loop do
       clear_terminal
       display_dealer_hand(true, false)
       display_human_hand
@@ -266,26 +270,26 @@ class Game
       else
         break
       end
-    end #HUMAN LOOP END
+    end
   end
-  
+
   def dealer_loop
-    loop do #DEALER LOOP 
+    loop do
       clear_terminal
       display_dealer_hand(false, true)
       break if dealer.bust? || dealer.have_17?
       hit(dealer)
       next_card
-    end #DEALER LOOP END
+    end
   end
 
   def main_loop
-    loop do 
+    loop do
       human_loop
       break if human.bust?
       dealer_loop
       break
-    end 
+    end
   end
 
   def play
@@ -301,7 +305,6 @@ class Game
     end
     display_goodbye_message
   end
-
 end
 
 Game.new.play
